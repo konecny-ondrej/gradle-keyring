5e2226b8aa5d768a91630a22b8a27f8b042c98c1
Ondřej Konečný <konecny.ondrej@gmail.com>
2024-04-26 17:47:09 +0200

    Publication config

--- gradle-keyring/build.gradle.kts
+++ gradle-keyring/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     // Apply the Java Gradle plugin development plugin to add support for developing Gradle plugins
     `java-gradle-plugin`
     `maven-publish`
+    `signing`
 
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     alias(libs.plugins.jvm)
+    alias(libs.plugins.pluginpublish)
 
     if (System.getProperty("useKeyring") != null) {
         alias(libs.plugins.keyring)
@@ -23,6 +25,10 @@ description = "Store/load your secrets to/from your system's keyring."
 keyring {
     secret("gradle.publish.key")
     secret("gradle.publish.secret")
+}
+
+signing {
+    useGpgCmd()
 }
 
 java {
@@ -54,10 +60,15 @@ dependencies {
 }
 
 gradlePlugin {
+    vcsUrl = "https://github.com/konecny-ondrej/gradle-keyring.git"
+    website = "https://github.com/konecny-ondrej/gradle-keyring"
     // Define the plugin
-    val greeting by plugins.creating {
+    val keyring by plugins.creating {
         id = libs.plugins.keyring.get().pluginId
         implementationClass = "me.okonecny.gradlekeyring.GradleKeyringPlugin"
+        displayName = "Gradle Keyring"
+        description = project.description
+        tags = listOf("keyring", "secret", "secrets", "secretservice", "credentials")
     }
 }
 
@@ -85,4 +96,11 @@ tasks.named<Task>("check") {
 tasks.named<Test>("test") {
     // Use JUnit Jupiter for unit tests.
     useJUnitPlatform()
+}
+
+tasks.named("publishPlugins").configure {
+    doFirst {
+        project.ext["gradle.publish.key"] = keyring.secrets["gradle.publish.key"]
+        project.ext["gradle.publish.secret"] = keyring.secrets["gradle.publish.secret"]
+    }
 }
